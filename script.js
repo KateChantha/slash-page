896f5a02ff62a64f70ab79f9b8b07976a8214751
Kato <kubkate@gmail.com>
2020-10-09 16:36:32 -0700

    revise changeBackground functionality to toggle back to the default background

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const {body} = document;
 
 function changeBackground(number) {
+  // Store prev className 
+  let prevBackground;
+  if (body.className) {
+    prevBackground = body.className;
+  }
   // Reset CSS class for body
   body.className = '';
   
+  // Check if same background is picked, then classname is toggled back to default background
   switch(number) {
     case '1':
-      body.classList.add('background-1')
-      break;
+      return (prevBackground === 'background-1'? false: body.classList.add('background-1'))
     case '2':
-      body.classList.add('background-2')
-      break;
+      return (prevBackground === 'background-2'? false: body.classList.add('background-2'))
     case '3':
-      body.classList.add('background-3')
-      break;
+      return (prevBackground === 'background-3'? false: body.classList.add('background-3'))
     default:
       break;
   }
